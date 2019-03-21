79d7f024815bf611b97010045d69f0c75e05fe0a
Sam McAlilly <smcalilly@gmail.com>
2019-03-20 23:30:26 -0500

    wooohooo it works

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
 chrome.runtime.sendMessage({todo: "showPageAction"});
+
+chrome.runtime.onMessage.addListener(function(request, send, sendResponse){
+  if (request.todo == "changeFont"){
+    var addFont = request.clickedFont;
+    $('p').css('font-family', addFont);
+  }
+});
--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ $(function(){
   });
   $('#btnChange').click(function(){
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-          chrome.tabs.sendMessage(tabs[0].id, {todo: "changeFont", clickedFont: font})
+          chrome.tabs.sendMessage(tabs[0].id, {todo: "changeFont", clickedFont: font});
       });
   });
 });
