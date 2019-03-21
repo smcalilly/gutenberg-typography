60d3a2bf6d3b7035dfbf15aa410c07cdca3324c6
Sam McAlilly <smcalilly@gmail.com>
2019-03-20 23:16:29 -0500

    adding popup selection function

---
+++ popup.js
@@ -0,0 +1,11 @@
+$(function(){
+  var font = $('#fontStyle').val();
+  $('#fontStyle').on("change paste keyup", function(){
+    font = $(this).val();
+  });
+  $('#btnChange').click(function(){
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+          chrome.tabs.sendMessage(tabs[0].id, {todo: "changeFont", clickedFont: font})
+      });
+  });
+});
