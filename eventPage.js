57664a6ec557304d7355cfe37346501518e38dec
Sam McAlilly <smcalilly@gmail.com>
2019-03-20 22:33:03 -0500

    updating eventPage.js to work on all the proper pages

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,7 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-  chrome.pageAction.show(tabs[0].id);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    if (request.todo == "showPageAction"){
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.pageAction.show(tabs[0].id);
+          });
+    }
 });
