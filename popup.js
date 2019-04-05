1bfb7151003deab4b6dee9155d078f2330b63920
Sam McAlilly <smcalilly@gmail.com>
2019-04-05 16:05:06 -0500

    cleanup

--- popup.js
+++ popup.js
@@ -5,14 +5,15 @@ $(function(){
     font = $(this).val();
   });
 
+  // set font size that corelates with line height and width
   $('#slider').on('change',function(){
     var val = $(this).val();
-    // set font size that corelates with line height and width
   });
+
   // set the user-generated font style and send to the content.js script
   $('#btnChange').click(function(){
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
           chrome.tabs.sendMessage(tabs[0].id, {todo: "changeFont", clickedFont: font});
-      });
+    });
   });
 });
